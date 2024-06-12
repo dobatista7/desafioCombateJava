18b68c6d9ae8d10ab2e586eabc03bd60f5e212ce
Douglas Batista <dobatista7@gmail.com>
2024-06-12 15:27:56 -0300

    entrada de dados campeao1, campeao2, turnos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,39 @@
 package application;
 
+import entities.Champion;
+
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite os dados do primeiro campeão: ");
+        System.out.print("Nome: ");
+        String name1 = sc.nextLine();
+        System.out.print("Vida inicial: ");
+        int life1 = sc.nextInt();
+        System.out.print("Ataque:  ");
+        int attack1 = sc.nextInt();
+        System.out.print("Armadura: ");
+        int armor1 = sc.nextInt();
+        sc.nextLine();
+        Champion champion1 = new Champion(name1, life1, attack1,armor1);
+
+        System.out.println();
+        System.out.println("Digite os dados do segundo campeão: ");
+        System.out.print("Nome: ");
+        String name2 = sc.nextLine();
+        System.out.print("Vida inicial: ");
+        int life2 = sc.nextInt();
+        System.out.print("Ataque:  ");
+        int attack2 = sc.nextInt();
+        System.out.print("Armadura: ");
+        int armor2 = sc.nextInt();
+        sc.nextLine();
+        Champion champion2 = new Champion(name1, life1, attack1,armor1);
+
 
         }
     }
